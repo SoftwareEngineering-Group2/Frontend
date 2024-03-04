55aa13d5bfbcef2b07f705584035c063abeef9cf
fwett <fwetterlov@gmail.com>
2024-03-04 16:47:19 +0100

    re-add, ts files were empty

---
+++ src/api/deviceService.ts
@@ -0,0 +1,13 @@
+import httpClient from "./httpClient";
+
+export const getDeviceState = (deviceType: string) => {
+  return httpClient.get(`/device/${deviceType}/state`);
+}
+
+export const updateDeviceState = (deviceType: string, newState: { state: string }) => {
+  return httpClient.post(`/device/${deviceType}`, newState);
+};
+
+export const getDeviceImage = (deviceType: string) => {
+  return httpClient.get(`/device/${deviceType}/image`);
+};
