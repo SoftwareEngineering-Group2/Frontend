357c221144d65836ddea484f065a0e914677b2b3
ludde94 <l.jenisch@hotmail.com>
2024-04-10 01:56:24 +0200

    Profilepage drawn up

--- src/screens/Profile/profileStyle.js
+++ src/screens/Profile/profileStyle.js
@@ -3,25 +3,61 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
-    position: 'relative',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#f0f0f0',
   },
   heading: {
     fontSize: 24,
-    fontWeight: 'bold',
     marginBottom: 20,
+    color: '#007bff',
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.4)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+    alignSelf: 'center',
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    width: '100%',
+    marginBottom: 20,
+    alignItems: 'flex-start',
   },
   profileInfo: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 10,
+    width: '100%',
   },
   label: {
+    fontSize: 18,
     fontWeight: 'bold',
     marginRight: 10,
+    color: '#007bff',
+  },
+  profileDetailText: {
+    fontSize: 18, 
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60, // Makes it a circle
+    marginBottom: 20,
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderColor: '#007bff',
   },
 });
 
-export default styles
+export default styles;
