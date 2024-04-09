ad2a92d831fd66f87d2533ccf93275847e0e6827
gabriel petersson <Gabriel@professionalnetwork.se>
2024-04-09 11:58:20 +0200

    details

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from 'react';
-
 import UserStack from './userStack';
 import AuthStack from './authStack';
-
 import { useAuthentication } from '../hooks/useAuth';
 
-
 export default function RootNavigation() {
-  
- /*  const token = useToken(); */  // Directly use the custom hook her
-
-  // This effect updates the authorization token whenever the token changes
-
-  // This assumes useAuthentication is defined elsewhere and working
   const { user } = useAuthentication();
+  
   return user ? <UserStack /> : <AuthStack />;
 }
