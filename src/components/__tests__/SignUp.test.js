dad49b0f3f6e7eaf3d2f0cc697f9ed0fbfc9cd5a
Fredrik Johnsson <fredrik.johnsson0029@stud.hkr.se>
2024-04-29 12:53:28 +0200

    First draft of setting page

--- src/components/__tests__/SignUp.test.js
+++ src/components/__tests__/SignUp.test.js
@@ -1,15 +1,24 @@
-import { signUp } from '../../../__mocks__/signUpLogic.mock';
+import { signUp } from '../../../__mocks__/signUpLogic.mock'
+
+it('should fail to sign up a user with invalid credentials', async () => {
+  const email = 'valid@example.com'
+  const password = 'invalidPassword'
+  const firstName = 'Johnny'
+  const lastName = 'Does'
+
+  await expect(signUp(email, password, firstName, lastName)).rejects.toThrow('Error creating user.')
+})
 
 it('should successfully sign up a user', async () => {
   const email = 'valid@example.com';
   const password = 'correctPassword';
-  const firstName = 'John'; // Provide a value for firstName
-  const lastName = 'Doe'; // Provide a value for lastName
+  const firstName = 'John'
+  const lastName = 'Doe'
 
   await expect(signUp(email, password, firstName, lastName)).resolves.toEqual({
     uid: '123',
     email: 'valid@example.com',
-    firstName: 'John', // Ensure that the expected firstName matches the provided value
-    lastName: 'Doe', // Ensure that the expected lastName matches the provided value
-  });
-});
+    firstName: 'John',
+    lastName: 'Doe',
+  })
+})
