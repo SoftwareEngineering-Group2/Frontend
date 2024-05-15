c340d1dc9307e4604fc6c44ca3e7620162a53c8b
gabriel petersson <Gabriel@professionalnetwork.se>
2024-05-15 11:45:33 +0200

    added curtain to voice and removed alert

--- src/screens/Home/handleSpeech.ts
+++ src/screens/Home/handleSpeech.ts
@@ -4,25 +4,25 @@ export default async function handleSpeech(text: string, setSpokenText: Function
   console.log("Spoken Text:", text);
   var words = text.toLowerCase();
   //white LED
-  if (words === "turn on white led") { 
+  if (words === "turn on white lamp") { 
     const newState = { state: true};
     // Update the device state in the backend
     await updateDeviceState("whiteLed", newState);
     setSpokenText(text);
-  }else if (words === "turn off white led"){
+  }else if (words === "turn off white lamp"){
     const newState = { state: false};
     // Update the device state in the backend
     await updateDeviceState("whiteLed", newState);
     setSpokenText(text);
   }
   //yellow LED
-  else if(words === "turn on yellow led"){
+  else if(words === "turn on yellow lamp"){
     console.log(words)
     const newState = { state: true};
     // Update the device state in the backend
     await updateDeviceState("yellowLed", newState);
     setSpokenText(text);
-  }else if(words === "turn off white led"){
+  }else if(words === "turn off yellow lamp"){
     const newState = { state: false};
     // Update the device state in the backend
     await updateDeviceState("yellowLed", newState);
@@ -74,6 +74,18 @@ export default async function handleSpeech(text: string, setSpokenText: Function
     const newState = { state: false};
     // Update the device state in the backend
     await updateDeviceState("fan", newState);
+    setSpokenText(text);
+  }
+  //Curtain
+  else if(words === "open curtain"){
+    const newState = { state: true};
+    // Update the device state in the backend
+    await updateDeviceState("curtain", newState);
+    setSpokenText(text);
+  }else if(words === "close curtain"){
+    const newState = { state: false};
+    // Update the device state in the backend
+    await updateDeviceState("curtain", newState);
     setSpokenText(text);
   }
 
